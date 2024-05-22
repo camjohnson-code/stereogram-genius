360ce3f26b9ce87880011068283f607f31d8f177
Florenzo Bauer <florenzobauer@gmail.com>
2024-05-22 10:16:06 -0600

    Added the Results and Pattern Pages.

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingPage from './Screens/LandingPage'
 import NewScreen from './Screens/NewScreen'
+import ResultsPage from './Screens/ResultsPage';
+import PatternsPage from './Screens/PatternsPage';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="ResultsPage">
         <Stack.Screen options={{ headerShown: false }} name="Home" component={LandingPage} />
         <Stack.Screen options={{ headerShown: false }} name="NewScreen" component={NewScreen} />
+        <Stack.Screen options={{headerShown: false }} name="ResultsPage" component={ResultsPage} />
+        <Stack.Screen options={{headerShown: false }} name="PatternsPage" component={PatternsPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
